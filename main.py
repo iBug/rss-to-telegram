2177540e9070fab5f24b0f02ac1ed64c263756e3
iBug <git@ibugone.com>
2021-04-08 14:34:48 +0800

    Use separate last_delivered record for each feed

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import json
 import os
 import re
 import time
+from collections import defaultdict
 
 import feedparser
 import telegram
+
+
+EXCLUDE_AUTHORS = ["github-actions[bot]"]
 
 
 def escape(s):
@@ -22,14 +26,16 @@ def main():
     bot = telegram.Bot(token=CONFIG['telegram_token'])
 
     DATA = {
-        'last_delivered': dateutil.parser.parse("1970-01-01T00:00:00Z"),
+        'last_delivered': defaultdict(lambda: dateutil.parser.parse("1970-01-01T00:00:00Z"))
     }
     if os.path.isfile("data.json"):
         with open("data.json", "r") as f:
             DATA = json.load(f)
+            if isinstance(DATA['last_delivered'], str):
+                last_delivered = dateutil.parser.parse(DATA['last_delivered'])
+                DATA['last_delivered'] = defaultdict(lambda: last_delivered)
 
     feed_list = CONFIG['feeds']
-    last_delivered = dateutil.parser.parse(DATA['last_delivered'])
     queue = []
 
     for item in feed_list:
@@ -41,11 +47,11 @@ def main():
                 feed['time'] = feed_time
                 queue.append((item['name'], feed))
 
-    queue.sort(key=lambda x: x[1]['time'])
     if queue:
+        queue.sort(key=lambda x: x[1]['time'])
         for name, feed in queue:
             author = feed['authors'][0]
-            if author['name'] == "github-actions[bot]":
+            if author['name'] in EXCLUDE_AUTHORS:
                 continue
             message = f"*\\[{escape(name)}\\]* {escape(feed['title'])}" \
                       f" \\([{escape(author['name'])}]({feed['link']})\\)"
@@ -53,8 +59,9 @@ def main():
                              text=message,
                              parse_mode="MarkdownV2",
                              disable_web_page_preview=True)
+            if dateutil.parser.parse(DATA['last_delivered'][name]) < feed['time']:
+                DATA['last_delivered'][name] = feed['time'].isoformat()
             time.sleep(1)
-        DATA['last_delivered'] = queue[-1][1]['time'].isoformat()
 
     with open("data.json", "w") as f:
         json.dump(DATA, f, indent=2, ensure_ascii=False)
